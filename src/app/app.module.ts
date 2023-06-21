b0b65083e918a7714146fe6576793cffecf998a1
Patricio Kanaszyc <patricio.kanaszyc@autologica.com>
2023-06-21 13:04:00 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainWrapperComponent } from './components/main-wrapper/main-wrapper.component';
+import { MyPComponent } from './components/my-p/my-p.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent,
+    MyPComponent
   ],
   imports: [
     BrowserModule
